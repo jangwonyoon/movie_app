a89197ba7be600436e5ae1c80c89e6ec3dc6f7b7
TheJang <33803975+jangwonyoon@gmail.com>
2020-04-24 11:01:39 +0900

    Updates

---
+++ precache-manifest.83d8fdb346ab6412b72765d8006b34e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3635c30435beaaab763f3df2e11fe7bc",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "9b4c1a0fe86fc9ddac38",
+    "url": "/movie_app/static/css/main.196d3b4e.chunk.css"
+  },
+  {
+    "revision": "98be54eaa9ed6a5a4f95",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4c1a0fe86fc9ddac38",
+    "url": "/movie_app/static/js/main.b5bd29f8.chunk.js"
+  },
+  {
+    "revision": "4dcb005dec2f66fe5231",
+    "url": "/movie_app/static/js/runtime-main.615d8c0e.js"
+  }
+]);
