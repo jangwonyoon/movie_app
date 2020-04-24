4e170cfb114c1a0a0cb97a28b7ec00745d9392b8
TheJang <33803975+jangwonyoon@gmail.com>
2020-04-24 12:02:06 +0900

    react-Router apply

--- src/App.js
+++ src/App.js
@@ -1,59 +1,36 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Nav from "./components/Nav";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoding: true,
-      movies: [],
-    };
-  }
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    this.setState({ movies, isLoding: false });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoding, movies } = this.state;
-
-    return (
-      <section className="container">
-        {isLoding ? (
-          <div className="loader">
-            <span className="loader__text">"Loding..."</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => {
-              return (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  title={movie.title}
-                  year={movie.year}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              );
-            })}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Nav />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" exact={true} component={About} />
+    </HashRouter>
+  );
 }
 
+// exact  -> 자신이 원하는 페이지만 라우팅 하게 만들어 주기
+
+// react router는 원래 path에 걸려 있는 부분을 확인 하기 때문에 exact를 써서
+// 원하는 페이지를 처리한다.
+
 export default App;
+
+/*
+여기서 라우터라는 것은 명령을 받고 실행해주는 역할이고 
+라우트라는 것은 경로와 경로에 맞는 컴포넌트를 적어서 라우터에게 명령을 내리는 역할인가요?
+*/
+
+// 라우터 안에서 링크를 사용
+
+// 라우터 밖에서는 링크 사용 불가
+
+// browserRouter 와 hashRouter 의 차이
+
+// 브라우저 라우터는 링크에 # 해시가 없다. 페이지 링크가 깔끔하다
+// 하지만 브라우저 라우터는 깃헙 페이지에 올리기가 힘들고 해쉬라우터는 편하다.
---
+++ src/routes/Home.js
@@ -0,0 +1,59 @@
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoding: true,
+      movies: [],
+    };
+  }
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies, isLoding: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoding, movies } = this.state;
+
+    return (
+      <section className="container">
+        {isLoding ? (
+          <div className="loader">
+            <span className="loader__text">"Loding..."</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  title={movie.title}
+                  year={movie.year}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default Home;
